b140d6e29b686fa389abf65d30ff2cff0e8f6977
Keith Cusson <keithcusson@gmail.com>
2025-10-14 09:47:05 -0300

    python-qgis.bat: Added folder that includes reference to processing module in python path.

--- python-qgis.bat
+++ python-qgis.bat
@@ -8,5 +8,5 @@ rem Set VSI cache to be used as buffer, see #6448
 set VSI_CACHE=TRUE
 set VSI_CACHE_SIZE=1000000
 set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
-set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis\python;%OSGEO4W_ROOT%\apps\qgis\python\plugins;%PYTHONPATH%
+set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis\python;%OSGEO4W_ROOT%\apps\qgis\python\plugins;%OSGEO4W_ROOT%\apps\grass\grass84\etc\python;%PYTHONPATH%
 python %*
